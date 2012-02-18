21e2c74632de522fbbaa08e435dae5268c9686dc
Tiratat <tiratat@tiratat-S6410.(none)>
2012-02-18 12:45:15 +0700

    Initial commit for webview

--- src/com/nuttyknot/whenwhere/WhenwhereActivity.java
+++ src/com/nuttyknot/whenwhere/WhenwhereActivity.java
@@ -2,12 +2,23 @@ package com.nuttyknot.whenwhere;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class WhenwhereActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private WebView browser;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		browser = (WebView) findViewById(R.id.webView);
+		browser.getSettings().setJavaScriptEnabled(true);
+		browser.addJavascriptInterface(this, "backend");
+		browser.loadUrl("file:///android_asset/main.htm");
+	}
+
+	public void create(String in) {
+		final String url = "file:///android_asset/www/" + in;
+	}
 }
