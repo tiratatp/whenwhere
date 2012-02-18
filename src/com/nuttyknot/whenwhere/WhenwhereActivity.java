f0a5f15993edd436361fd1f1ade690781a76edb4
Tiratat <tiratat@tiratat-S6410.(none)>
2012-02-18 10:31:38 +0700

    Initial Commit

---
+++ src/com/nuttyknot/whenwhere/WhenwhereActivity.java
@@ -0,0 +1,13 @@
+package com.nuttyknot.whenwhere;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WhenwhereActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
